880ab331efaf6478fc6e52ca3867570e9d55c4ba
Oliver Zeigermann <oliver.zeigermann@gmail.com>
2019-07-22 18:44:06 +0200

    process wip

--- code/server.py
+++ code/server.py
@@ -6,23 +6,24 @@ from prediction import predict
 app = Flask(__name__)
 CORS(app)
 
-@app.route('/', methods=['GET', 'POST'])
+@app.route("/ping")
+def ping():
+    return "pong"
+
+@app.route('/predict', methods=['GET', 'POST'])
 def do_predict():
     speed = request.json['speed']
     age = request.json['age']
     miles = request.json['miles']
 
-    predicted_category, probabilities, classes = predict(speed, age, miles)
-    # print ('predicted_category:', predicted_category)
-    # print ('prediction:', prediction)
+    predicted_category, probabilities = predict(speed, age, miles)
 
     response = {
         'category': predicted_category,
         'prediction': probabilities,
-        'classes': classes
     }
     return jsonify(response)
 
 if __name__ == '__main__':
-    app.run(debug=True, host='0.0.0.0', port=8889)
+    app.run(debug=True, host='0.0.0.0', port=80)
 
