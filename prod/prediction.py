08fb0277aa416ba10062a4dde555aad4b87cdc48
Oliver Zeigermann <oliver.zeigermann@gmail.com>
2019-07-25 12:57:07 +0200

    prod wip

--- prod/prediction.py
+++ prod/prediction.py
@@ -1,16 +1,9 @@
 import pickle
 
 # loading models only once on startup
-model = pickle.load(open('models/rf.model', 'rb'))
-
-# speed_std_scale = pickle.load(open('models/speed_std_scale.model', 'rb'))
-# age_std_scale = pickle.load(open('models/age_std_scale.model', 'rb'))
-# miles_std_scale = pickle.load(open('models/miles_std_scale.model', 'rb'))
+model = pickle.load(open('models/model.pickle', 'rb'))
 
 def predict(speed, age, miles):
-#     sample = [[speed_std_scale.transform([speed])[0],
-#                age_std_scale.transform([age])[0],
-#                miles_std_scale.transform([miles])[0]]]
     sample = [[speed, age, miles]]
 
     result = int(model.predict(sample)[0])
